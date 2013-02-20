7059ed6e749a702f89555c9c28488c556397b41a
Allen Short <ashort@mozilla.com>
2013-02-20 12:14:29 -0800

    Precedence-aware expression unparser.

--- first.py
+++ first.py
@@ -130,219 +130,67 @@ class PythonParser(BootOMetaGrammar.makeGrammar(g, globals())):
         return atom
 
 
-class PythonWriter(object):
+class PrecedenceTemplate(object):
+    def __init__(self, prec, contents):
+        self.prec = prec
+        self.contents = contents
 
-    indent = 0
+    def __str__(self):
+        parts = []
+        for item in self.contents:
+            if isinstance(item, basestring):
+                parts.append(item)
+            else:
+                prec = getattr(item, 'prec', None)
+                if prec is None:
+                    prec = -float('inf')
+                if prec > self.prec:
+                    parts.append("(%s)" % (item,))
+                else:
+                    parts.append(str(item))
+        return ''.join(parts)
 
-    def __init__(self):
-        self.parts = []
+    def __repr__(self):
+        return "<PT %s %r>" % (self.prec, self.contents)
 
-    def write(self, statements):
-        for statement in statements:
-            self._write(statement)
 
-    def _write(self, t):
-        if isinstance(t, list):
-            for s in t:
-                self._write(s)
-        else:
-            self.term(t)
+class PrecedenceTransformer(TreeTransformerBase):
+    nextPrecedence = None
 
-    def start_line(self):
-        self.parts.append(" " * self.indent)
+    def rule_prec(self, prec):
+        self.nextPrecedence = prec
+        return None, self.input.nullError()
 
-    def end_line(self):
-        self.parts.append("\n")
+    def stringtemplate(self, template, vals):
+        output = []
+        for chunk in template.args:
+            if chunk.tag.name == ".String.":
+                output.append(chunk.data)
+            elif chunk.tag.name == "QuasiExprHole":
+                v = vals[chunk.args[0].data]
+                output.append(v)
+            else:
+                raise TypeError("didn't expect %r in string template" % chunk)
 
-    def term(self, t):
-        getattr(self, "term_%s" % t.tag.name)(t)
+        pt = PrecedenceTemplate(self.nextPrecedence, output)
+        self.nextPrecedence = None
+        return pt
 
-    def term_Assign(self, t):
-        self.start_line()
-        # XXX doesn't support lvalue unpacks
-        self.term(t.args[0].args[0])
-        self.parts.append(" = ")
-        self.term(t.args[1])
-        self.end_line()
+PythonExpressionUnparser = TreeTransformerGrammar.makeGrammar(
+    open("expression_unparser.parsley").read(), globals(),
+    'PythonExpressionUnparser', superclass=PrecedenceTransformer)
 
-    def term_Attribute(self, t):
-        self.term(t.args[0])
-        self.parts.append(".")
-        self.parts.append(t.args[1].data)
-
-    def term_Call(self, t):
-        self.term(t.args[0])
-        # XXX doesn't support actually emitting parameters to calls yet
-        self.parts.append("()")
-
-    def term_Class(self, t):
-        self.start_line()
-        self.parts.append("class ")
-        self.parts.append(t.args[0].data)
-        self.parts.append("(")
-        for parent in t.args[1:-1]:
-            self.term(parent)
-        self.parts.append("):")
-        self.end_line()
-        self.indent += 1
-        for statement in t.args[-1].args:
-            self.term(statement)
-        self.indent -= 1
-
-    def term_Def(self, t):
-        self.start_line()
-        self.parts.append("def ")
-        self.parts.append(t.args[0].data)
-        # XXX doesn't insert parameters
-        self.parts.append("():")
-        self.end_line()
-        self.indent += 1
-        for statement in t.args[-1].args:
-            self.term(statement)
-        self.indent -= 1
-
-    def term_If(self, t):
-        self.start_line()
-        self.parts.append("if ")
-        self.term(t.args[0])
-        self.parts.append(":")
-        self.end_line()
-        self.indent += 1
-        for statement in t.args[1].args:
-            self.term(statement)
-        self.indent -= 1
-        # XXX else?
-
-    def term_Name(self, t):
-        self.parts.append(t.args[0].data)
-
-    def term_Not(self, t):
-        self.parts.append("(not ")
-        self.term(t.args[0])
-        self.parts.append(")")
-
-    def term_Num(self, t):
-        self.parts.append(str(t.args[0].data))
-
-    def term_Pass(self, t):
-        self.start_line()
-        # I think we can end the line ourselves, thanks. :3
-        self.parts.append("pass\n")
-
-    def term_Return(self, t):
-        self.start_line()
-        self.parts.append("return ")
-        self.term(t.args[0])
-        self.end_line()
-
-    def term_Str(self, t):
-        if t.args[0].data:
-            self.parts.append(" %s" % (t.args[0].data,))
-        self.parts.append(repr(t.args[1].data))
-
-    def term_Subscript(self, t):
-        self.term(t.args[0])
-        self.parts.append("[")
-        self.term(t.args[1])
-        self.parts.append("]")
-
-    def term_Tuple(self, t):
-        self.parts.append("(")
-        for element in t.args:
-            self.term(element)
-            self.parts.append(",")
-        self.parts.append(")")
-
-unparser = r"""
-assign_lhs_part = (Attribute(assign_lhs_part:o @attr) --> $o.$attr
-                  |Subscript(assign_lhs_part:o @sub) --> $o[$sub]
-                  |Call(assign_lhs_part:o @args) --> $o($args)
-                  |Name(@n) -> n
-                  )
-assign_lhs = Tuple(assign_lhs_part*:parts) -> ', '.join(parts)
-multi_assign = assign_lhs+:lhs -> " = ".join(lhs)
-#Assign([multi_assign:lhss] @rhs) --> $lhss = $rhs
-Assign([assign_lhs_part:lhss] @rhs) --> $lhss = $rhs
-
-Attribute(@expr @name) --> $expr.$name
-
-Call(@expr null) --> $expr()
-Call(@expr @args) --> $expr($args)
-
-Class(@name null @suite) {{{
-class $name:
-    $suite
-}}}
-Class(@name (@p | cls_parents:p) @suite) {{{
-class $name($p):
-    $suite
-}}}
-cls_parents Tuple(transform*:es) -> ", ".join(es)
-
-Def(@name @params @suite) {{{
-def $name($params):
-    $suite
-}}}
-
-Parameters() -> ""
-
-elifs = [@ele @elthen] {{{
-elif $ele:
-    $elthen
-}}}
-If(@test @consq [elifs*]:els null) {{{
-if $test:
-    $consq
-$els
-}}}
-If(@test @consq [elifs*]:els @alt) {{{
-if $test:
-    $consq
-$els
-else:
-    $alt
-}}}
-
-Name(@n) -> n
-Not(@expr) --> (not $expr)
-Num(@n) -> str(n)
-Pass() --> pass
-Return(@expr) --> return $expr
-Str(@prefix @s) -> prefix + repr(s)
-Subscript(@expr @sub) --> $expr[$sub]
-
-Tuple(@single) --> ($single,)
-Tuple(transform*:elts) -> '(' + ', '.join(elts) + ')'
-
-File([transform*:stmts]) {{{
-$stmts
-}}}
-"""
-
-PythonUnparser = TreeTransformerGrammar.makeGrammar(
-    unparser, globals(),
-    'PythonUnparser', superclass=TreeTransformerBase)
-
+PythonStatementUnparser = TreeTransformerGrammar.makeGrammar(
+    open("statement_unparser.parsley").read(), globals(),
+    'PythonStatementUnparser', superclass=TreeTransformerBase)
 
 
 if __name__ == "__main__":
-    f = open(sys.argv[1]).read()
+    from terml.parser import parseTerm as term
+    # f = open(sys.argv[1]).read()
     g = wrapGrammar(PythonParser)
-    stmts = g(f).file_input()
+    stmts = g(sys.argv[1] + '\n').file_input()
     from pprint import pprint
     pprint(stmts)
-    print PythonUnparser.transform(stmts)[0]
-    # pw = PythonWriter()
-    # pw.write(stmts)
-    # print pw.parts
-    # print "".join(pw.parts)
-
-
-
-
-
-
-
-
-
-
+    pt = PythonExpressionUnparser.transform(stmts)[0]
+    print pt
