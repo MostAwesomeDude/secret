4ff937acf2d6b6337abb1b8dcee5f40f679df8fd
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-04-19 21:55:53 -0700

    run: Six by seven is forty-two.

--- s/run.py
+++ s/run.py
@@ -1,8 +1,6 @@
 import sys
 
 
-SHOW = -1
-
 DROP = 0
 DUP = 1
 SWAP = 2
@@ -14,6 +12,9 @@ SEND = 6
 GET = 7
 SET = 8
 NEW = 9
+
+MAKE_LIST = 10
+SHOW = 11
 
 
 class Object(object):
@@ -87,10 +88,32 @@ class Int(Object):
         self._i = i
 
     def call(self, message, arguments):
-        pass
+        assert isinstance(message, Str)
+        m = message._s
+
+        if m == "__mul__":
+            assert isinstance(arguments, List)
+            assert len(arguments._l) == 1
+            arg = arguments._l[0]
+            assert isinstance(arg, Int)
+            return Int(self._i * arg._i)
+
+        return Object.call(self, message, arguments)
 
     def __str__(self):
         return str(self._i)
+
+
+class List(Object):
+    """
+    A boxed list.
+    """
+
+    def __init__(self, l):
+        self._l = l
+
+    def __str__(self):
+        return str(self._l)
 
 
 class Stack(object):
@@ -125,11 +148,9 @@ def eval(bc, frame, scope):
         inst = bc[pc]
 
         print "pc: %d inst: %d" % (pc, inst)
-        print "tos: %s" % str(stack.peek())
+        print "depth: %d tos: %s" % (len(stack._storage), stack.peek().__str__())
 
-        if inst == SHOW:
-            print stack.peek().__str__()
-        elif inst == DROP:
+        if inst == DROP:
             stack.drop()
         elif inst == DUP:
             stack.push(stack.peek())
@@ -148,6 +169,8 @@ def eval(bc, frame, scope):
             args = stack.pop()
             message = stack.pop()
             target = stack.pop()
+            print "call: args: %s message: %s target: %s" % (str(args),
+                    str(message), str(target))
             rv = target.call(message, args)
             stack.push(rv)
         elif inst == SEND:
@@ -158,18 +181,42 @@ def eval(bc, frame, scope):
             pass # XXX
         elif inst == NEW:
             pass # XXX
+        elif inst == MAKE_LIST:
+            pc += 1
+            length = bc[pc]
+            l = []
+            i = 0
+            while i < length:
+                l.append(stack.pop())
+                i += 1
+            stack.push(List(l))
+        elif inst == SHOW:
+            print stack.peek().__str__()
 
         pc += 1
 
 
 def run(bc):
-    frame = [Int(42)]
+    frame = [Int(6), Int(7), Str("__mul__")]
     scope = {}
     return eval(bc, frame, scope)
 
 
 def entry_point(argv):
-    run([PUSH_CONST, 0, SHOW])
+    run([
+        # Push 6.
+        PUSH_CONST, 0,
+        # Push 7.
+        PUSH_CONST, 1,
+        # 7 -> [7].
+        MAKE_LIST, 1,
+        # Push "__mul__".
+        PUSH_CONST, 2,
+        # (6).__mul__([7]).
+        SWAP, CALL,
+        # Show.
+        SHOW,
+    ])
     return 0
 
 
