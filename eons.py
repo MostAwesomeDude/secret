02253fcabd364a9bb4c3c5a861ba0d8a5f510632
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-06-16 10:13:02 -0700

    eons: Start building the actual interpreter.

--- eons.py
+++ eons.py
@@ -1,6 +1,3 @@
-def split_tokens(s):
-    return s.split(" ")
-
 builtins = {
     "()":      (0, 1),
     ".":       (3, 1),
@@ -34,6 +31,48 @@ def infer_stack_effect(tokens):
             o += 1
 
     return i, o
+
+
+def execute(token, stack, context):
+    if False:
+        pass
+    elif token == "()":
+        stack.push([])
+    elif token == ".":
+        args = stack.pop()
+        name = stack.pop()
+        target = stack.pop()
+        result = pass_message(target, name, args)
+        stack.push(result)
+    elif token == "<-":
+        args = stack.pop()
+        name = stack.pop()
+        target = stack.pop()
+        # XXX wrong
+        stack.push((target, name, args))
+    elif token == "<method":
+        name = stack.pop()
+        code = stack.pop()
+        stack.push((name, code))
+    elif token == "drop":
+        stack.pop()
+    elif token == "dup":
+        stack.push(stack.peek())
+    elif token == "object":
+        methods = stack.pop()
+        d = dict(methods)
+        stack.push(d)
+    elif token == "push":
+        obj = stack.pop()
+        l = stack.peek()
+        l.append(obj)
+    elif token == "print":
+        print stack.pop()
+    elif token == "swap":
+        x = stack.pop()
+        y = stack.pop()
+        stack.push(x)
+        stack.push(y)
 
 
 def parse_pieces(data):
