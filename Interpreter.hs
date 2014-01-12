8e87a1ba94c7c2b4b622b2edce26c1b6fd777074
Corbin Simpson <cds@corbinsimpson.com>
2014-01-12 00:22:50 -0800

    Implement the structure for calls.
    Actually performing calls will require a tad more structure.

--- Interpreter.hs
+++ Interpreter.hs
@@ -6,14 +6,16 @@ module Interpreter where
 import Control.Lens
 import Control.Monad.Free
 import Control.Monad.State
+import Prelude hiding (drop)
 
 data Inst a = Dup a
             | Rot a
-            | Pop a
+            | Drop a
             | Swap a
             | GetSlot Int a
             | PutSlot Int a
             | Literal Int a
+            | Call String Int a
     deriving (Eq, Functor, Show)
 
 data Slot = Slot | NoSlot
@@ -36,8 +38,8 @@ makeLenses ''Environment
 dup :: Free Inst ()
 dup = liftF $ Dup ()
 
-pop :: Free Inst ()
-pop = liftF $ Pop ()
+drop :: Free Inst ()
+drop = liftF $ Drop ()
 
 swap :: Free Inst ()
 swap = liftF $ Swap ()
@@ -47,17 +49,25 @@ eval inst env = flip execState env $ iterM go inst
     where
     go (Dup ma) = stack %= (\(x:xs) -> x:x:xs) >> ma
     go (Rot ma) = stack %= (\(x:y:z:xs) -> y:z:x:xs) >> ma
-    go (Pop ma) = stack %= (\(x:xs) -> xs) >> ma
+    go (Drop ma) = stack %= (\(x:xs) -> xs) >> ma
     go (Swap ma) = stack %= (\(x:y:xs) -> y:x:xs) >> ma
     go (GetSlot i ma) = do
         Just s <- preuse $ slots . ix i
         stack %= (SlotObj s:)
         ma
     go (PutSlot i ma) = do
-        SlotObj s <- uses stack head
+        (SlotObj s:st) <- use stack
+        stack .= st
         slots . ix i .= s
         ma
     go (Literal i ma) = do
         Just l <- preuse $ literals . ix i
         stack %= (l:)
+        ma
+    go (Call verb arity ma) = do
+        obj:st <- use stack
+        let (args, st') = splitAt arity st
+        stack .= st'
+        -- XXX perform the actual call
+        stack %= (StrObj "Result goes here":)
         ma
