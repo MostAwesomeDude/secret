2c7bcec543825ff2a7a0defdf909338107de1ad9
Corbin Simpson <cds@corbinsimpson.com>
2014-01-11 23:10:10 -0800

    Interpret using State.
    Far easier to reason about and easier to use with lens.

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Interpreter where
 
+import Control.Lens
 import Control.Monad.Free
+import Control.Monad.State
 
 data Inst a = Dup a
             | Rot a
@@ -10,19 +13,25 @@ data Inst a = Dup a
             | Swap a
             | GetSlot Int a
             | PutSlot Int a
+            | Literal Int a
     deriving (Eq, Functor, Show)
 
 data Slot = Slot | NoSlot
     deriving (Eq, Show)
 
 data Object = SlotObj Slot
+            | StrObj String
+            | IntObj Integer
     deriving (Eq, Show)
 
 data Environment a = Env
-                   { slots :: [Slot]
-                   , stack :: [a]
+                   { _literals :: [Object]
+                   , _slots :: [Slot]
+                   , _stack :: [a]
                    }
     deriving (Eq, Show)
+
+makeLenses ''Environment
 
 dup :: Free Inst ()
 dup = liftF $ Dup ()
@@ -34,16 +43,21 @@ swap :: Free Inst ()
 swap = liftF $ Swap ()
 
 eval :: Free Inst () -> Environment Object -> Environment Object
-eval inst env = go env inst
+eval inst env = flip execState env $ iterM go inst
     where
-    go env (Free action) = go' env action
-    go env _ = env
-    go' env@(Env {stack = (a:as)}) (Dup cont) = go (env {stack = (a:a:as) }) cont
-    go' env@(Env {stack = (a:b:c:as)}) (Rot cont) = go (env {stack = (b:c:a:as) }) cont
-    go' env@(Env {stack = (a:as)}) (Pop cont) = go (env {stack = as }) cont
-    go' env@(Env {stack = (a:b:as)}) (Swap cont) = go (env {stack = (b:a:as) }) cont
-    go' (Env ss st) (GetSlot i cont) = let s = ss !! i in
-        go (Env ss (SlotObj s:st)) cont
-    go' (Env ss (SlotObj s:st)) (PutSlot i cont) = let ss' = take i ss ++ (s:drop (i+1) ss) in
-        go (Env ss' st) cont
-    go' env inst = error $ "Stack error: " ++ show inst ++ " Stack: " ++ show env
+    go (Dup ma) = stack %= (\(x:xs) -> x:x:xs) >> ma
+    go (Rot ma) = stack %= (\(x:y:z:xs) -> y:z:x:xs) >> ma
+    go (Pop ma) = stack %= (\(x:xs) -> xs) >> ma
+    go (Swap ma) = stack %= (\(x:y:xs) -> y:x:xs) >> ma
+    go (GetSlot i ma) = do
+        Just s <- preuse $ slots . ix i
+        stack %= (SlotObj s:)
+        ma
+    go (PutSlot i ma) = do
+        SlotObj s <- uses stack head
+        slots . ix i .= s
+        ma
+    go (Literal i ma) = do
+        Just l <- preuse $ literals . ix i
+        stack %= (l:)
+        ma
