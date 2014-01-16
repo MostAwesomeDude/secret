5c04359497cae7a18a30a36c7d57ed72e341d172
Corbin Simpson <cds@corbinsimpson.com>
2014-01-16 13:50:51 -0800

    Make scripts work.

--- Interpreter.hs
+++ Interpreter.hs
@@ -15,28 +15,50 @@ data Inst a = Dup a
             | GetSlot Int a
             | PutSlot Int a
             | Literal Int a
+            | BindObject Int Int a
             | Call String Int a
     deriving (Eq, Functor, Show)
 
-data Slot = Slot | NoSlot
+type Insts = Free Inst ()
+
+data Script = Script [(String, Int, Insts)]
     deriving (Eq, Show)
 
-data Object = SlotObj Slot
+data Object = ScriptObj Script Frame
+            | SlotObj (Maybe Object)
             | StrObj String
             | IntObj Integer
     deriving (Eq, Show)
 
+data Frame = Frame
+           { _scripts :: [(Script, Int)]
+           , _literals :: [Object]
+           , _slots :: [Maybe Object]
+           }
+    deriving (Eq, Show)
+
 data Environment a = Env
-                   { _literals :: [Object]
-                   , _slots :: [Slot]
+                   { _frame :: Frame
                    , _stack :: [a]
                    }
     deriving (Eq, Show)
 
 makeLenses ''Environment
+makeLenses ''Frame
 
-call :: Object -> String -> Int -> [Object] -> Object
-call (IntObj i) verb arity args = intCall i verb arity args
+findScript :: [(String, Int, Insts)] -> String -> Int -> Maybe Insts
+findScript ((verb', arity', insts):ss) verb arity
+    | verb == verb' && arity == arity' = Just insts
+    | otherwise = findScript ss verb arity
+findScript [] _ _ = Nothing
+
+call :: Object -> String -> Int -> [Object] -> State (Environment Object) ()
+call (ScriptObj (Script ss) frame) verb arity args =
+    case findScript ss verb arity of
+        Just insts -> modify $ eval insts
+        Nothing    -> fail "Method not found"
+call (IntObj i) verb arity args = push $ intCall i verb arity args
+    where push obj = stack %= (obj:)
 
 intCall :: Integer -> String -> Int -> [Object] -> Object
 intCall i "mul" 1 [IntObj j] = IntObj $ i * j
@@ -58,23 +80,32 @@ eval inst env = flip execState env $ iterM go inst
     go (Drop ma) = stack %= (\(x:xs) -> xs) >> ma
     go (Swap ma) = stack %= (\(x:y:xs) -> y:x:xs) >> ma
     go (GetSlot i ma) = do
-        Just s <- preuse $ slots . ix i
+        Just s <- preuse $ frame . slots . ix i
         stack %= (SlotObj s:)
         ma
     go (PutSlot i ma) = do
         (SlotObj s:st) <- use stack
         stack .= st
-        slots . ix i .= s
+        frame . slots . ix i .= s
         ma
     go (Literal i ma) = do
-        Just l <- preuse $ literals . ix i
+        Just l <- preuse $ frame . literals . ix i
         stack %= (l:)
+        ma
+    go (BindObject scriptIx slotIx ma) = do
+        Just (script, len) <- preuse $ frame . scripts . ix scriptIx
+        st <- use stack
+        let (auditors, st') = splitAt len st
+            (vars, st'')    = splitAt len st'
+            obj = ScriptObj script (Frame [] vars [])
+        stack .= obj:st''
+        frame . slots . ix slotIx .= Just obj
         ma
     go (Call verb arity ma) = do
         obj:st <- use stack
         let (args, st') = splitAt arity st
         stack .= st'
-        stack %= (call obj verb arity args:)
+        call obj verb arity args
         ma
 
 test :: Free Inst ()
@@ -83,8 +114,18 @@ test = do
     liftF $ Literal 1 ()
     liftF $ Call "mul" 1 ()
 
+test' :: Free Inst ()
+test' = do
+    liftF $ Literal 0 ()
+    liftF $ Literal 1 ()
+    liftF $ BindObject 0 0 ()
+    liftF $ Call "run" 2 ()
+
+mulScript :: Script
+mulScript = Script [("run", 2, test)]
+
 main :: IO ()
 main = print env'
     where
-    env = Env [IntObj 6, IntObj 7] [] []
-    env' = eval test env
+    env = Env (Frame [(mulScript, 0)] [IntObj 6, IntObj 7] []) []
+    env' = eval test' env
